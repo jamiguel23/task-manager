fc2b08809e3acd667b1e076505891d9957fdbb1f
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-08-29 15:23:26 -0700

    added test hit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+'use strict'
+
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/test', (req, res) => {
+  res.send(`test hit!`)
+});
+
+app.listen(PORT, () => {
+  
+  console.log(`Task Manager App listening on port ${PORT}` );
+});
